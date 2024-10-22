721f5f9b39ec88272b500935764f5a2bc7fe610e
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2024-10-22 10:50:52 -0300

    grafico vs peso

--- TP2/pendulo.py
+++ TP2/pendulo.py
@@ -38,13 +38,26 @@ def plot_all_trayectories(thetas, angles=[10, 15, 25, 45, 60]):
     plt.show()
 
 def plot_trayectory_based_lenght(thetas):
-    colors = ['r', 'b', 'g']
+    colors = ['r', 'b', 'black']
     lengths = [15, 26, 41.9]
     for theta in thetas:
-        plt.plot(theta, label=f"L = {lengths[thetas.index(theta)]} cm")
+        theta_plot = theta[:250]
+        plt.plot(theta_plot, label=f"L = {lengths[thetas.index(theta)]} cm", color=colors[thetas.index(theta)])
     plt.xlabel('Tiempo (ms)')
     plt.ylabel('Ángulo (°)')
     plt.title('Ángulo vs Largo')
+    plt.legend()
+    plt.show()
+
+def plot_trayectory_based_weight(thetas):
+    colors = ['r', 'b', 'black']
+    weights = [5, 23, 72]
+    for theta in thetas:
+        theta_plot = theta [:200]
+        plt.plot(theta_plot, label=f"m = {weights[thetas.index(theta)]} g", color=colors[thetas.index(theta)])
+    plt.xlabel('Tiempo (ms)')
+    plt.ylabel('Ángulo (°)')
+    plt.title('Ángulo vs Peso')
     plt.legend()
     plt.show()
 
@@ -57,11 +70,11 @@ def main():
     t45, r45, theta45 = get_data('tp2_fisica - angulo_45.csv')
     t60, r60, theta60 = get_data('tp2_fisica - angulo_60.csv')
 
-    plot_trayectory(theta10[56:], 10)
-    plot_trayectory(theta15[86:], 15)
-    plot_trayectory(theta25[73:], 25)
-    plot_trayectory(theta45[99:], 45)
-    plot_trayectory(theta60, 60)
+    # plot_trayectory(theta10[56:], 10)
+    # plot_trayectory(theta15[86:], 15)
+    # plot_trayectory(theta25[73:], 25)
+    # plot_trayectory(theta45[99:], 45)
+    # plot_trayectory(theta60, 60)
 
     # print(theta60.index(max(theta60)))
     # print(theta10.index(max(theta10)))
@@ -69,7 +82,7 @@ def main():
     # print(theta25.index(max(theta25)))
     # print(theta45.index(max(theta45)))
 
-    plot_all_trayectories([theta10[56:306], theta15[86:336], theta25[73:326], theta45[99:349], theta60[:250]])
+    # plot_all_trayectories([theta10[56:306], theta15[86:336], theta25[73:326], theta45[99:349], theta60[:250]])
 
     tl15, rl15, thetal15 = get_data('tp2_fisica - largo_15.csv')
     tl26, rl26, thetal26 = get_data('tp2_fisica - largo_26.csv')
@@ -77,7 +90,15 @@ def main():
     # print(thetal15.index(max(thetal15)))
     # print(thetal26.index(max(thetal26)))
 
-    plot_trayectory_based_lenght([thetal15[46:], thetal26[51:], theta25[70:]])
+    # plot_trayectory_based_lenght([thetal15[46:], thetal26[51:], theta25[70:]])
+
+    tw5, rw5, thetaw5 = get_data('tp2_fisica - peso_madera.csv')
+    tw72, rw72, thetaw72 = get_data('tp2_fisica - peso_oro.csv')
+
+    # print(thetaw5.index(max(thetaw5)))
+    # print(thetaw72.index(max(thetaw72)))
+
+    plot_trayectory_based_weight([thetaw5[30:], theta25[70:], thetaw72[179:]])
 
 if __name__ == '__main__':
     main()
