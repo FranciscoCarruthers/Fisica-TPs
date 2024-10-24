3e768f5f02c0841c516941665d0686cad1da9d1b
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2024-10-24 19:32:57 -0300

    millones de cambios

--- TP2/pendulo.py
+++ TP2/pendulo.py
@@ -1,5 +1,7 @@
 from operator import index
 
+from arrow import get
+from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,6 +20,42 @@ def get_data(file_path):
                 theta.append(float(line[2]))
 
         return t, r, theta
+    
+def get_avg_periodo_time(times, theta):
+    p = []
+    for i in range(len(theta)):
+        if abs(theta[i]) <= 1:
+            p.append(times[i])
+    
+    t = []
+    for i in range(len(p)-2):
+        t.append(p[i+2] - p[i])
+    
+    return np.mean(t)
+
+def linear_func(x, a, b):
+        return a * x + b
+
+def get_gravedad_local(T, times, thetas):
+    l = [0.15, 0.26, 0.42]
+    T = []
+    for i in range(len(thetas)):
+        T.append(get_avg_periodo_time(times[i], thetas[i])**2)
+
+    params, p_cov = curve_fit(linear_func, l, T, sigma=0.05*np.ones_like(T), absolute_sigma=True)
+    plt.scatter(l, T)
+    plt.plot(l, linear_func(np.array(l), *params))
+    plt.xlabel('Largo (m)')
+    plt.ylabel('$T^2$ ($s^2$)')
+    plt.legend()
+    plt.savefig('TP2/gravedad.png')
+    plt.show()
+
+    gravedad = 4 * np.pi**2 / params[0]
+    error_gravedad = gravedad * np.sqrt(p_cov[0, 0]) / params[0]
+
+    print('Gravedad local:', gravedad)
+    print('Error de la gravedad:', error_gravedad)
 
 def plot_trayectory(theta, angle):
     plt.plot(theta, label=f"Ángulo inicial = {angle}°")
@@ -38,17 +76,35 @@ def plot_all_trayectories(thetas, angles=[10, 15, 25, 45, 60]):
     plt.savefig('angulos.png')
     plt.show()
 
-def plot_trayectory_based_lenght(thetas):
+def cuadratic_func(x, a, b, c):
+    return a * x**2 + b * x + c
+
+def plot_trayectory_based_lenght(times, thetas):
     colors = ['r', 'b', 'black']
-    lengths = [15, 26, 42]
+    lengths = [0.15, 0.26, 0.42]
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
     for theta in thetas:
-        theta_plot = theta[:250]
-        plt.plot(theta_plot, label=f"L = {lengths[thetas.index(theta)]} cm", color=colors[thetas.index(theta)])
-    plt.xlabel('Tiempo (ms)')
+        theta_plot = theta[:175]
+        plt.plot(theta_plot, label=f"L = {lengths[thetas.index(theta)]} m", color=colors[thetas.index(theta)])
+    plt.xlabel('Tiempo (s)')
     plt.ylabel('Ángulo (°)')
     # plt.title('Ángulo vs Largo')
+
+    plt.subplot(1, 2, 2)
+    w = []
+    for i in range(len(thetas)):
+        w.append(1/get_avg_periodo_time(times[i], thetas[i]))
+
+    params, p_cov = curve_fit(cuadratic_func, lengths, w, sigma=0.05*np.ones_like(w), absolute_sigma=True)
+    plt.scatter(lengths, w)
+    plt.plot(lengths, cuadratic_func(np.array(lengths), *params))
+    plt.xlabel('Largo (m)')
+    plt.ylabel('Frecuencia (Hz)')
+    # plt.title('Frecuencia vs Largo')
+
     plt.legend()
-    plt.savefig('largo.png')
+    plt.savefig('TP2/largo.png')
     plt.show()
 
 def plot_trayectory_based_weight(thetas):
@@ -70,6 +126,7 @@ def main():
     t10, r10, theta10 = get_data('TP2/tp2_fisica - angulo_10.csv')
     t15, r15, theta15 = get_data('TP2/tp2_fisica - angulo_15.csv')
     t25, r25, theta25 = get_data('TP2/tp2_fisica - caso_base.csv')
+    t25 = [a-1 for a in t25]
     t45, r45, theta45 = get_data('TP2/tp2_fisica - angulo_45.csv')
     t60, r60, theta60 = get_data('TP2/tp2_fisica - angulo_60.csv')
 
@@ -85,23 +142,21 @@ def main():
     # print(theta25.index(max(theta25)))
     # print(theta45.index(max(theta45)))
 
-    plot_all_trayectories([theta10[56:306], theta15[86:336], theta25[73:326], theta45[99:349], theta60[:250]])
+    # plot_all_trayectories([theta10[56:306], theta15[86:336], theta25[73:326], theta45[99:349], theta60[:250]])
 
     tl15, rl15, thetal15 = get_data('TP2/tp2_fisica - largo_15.csv')
+    tl15 = [a-1 for a in tl15]
     tl26, rl26, thetal26 = get_data('TP2/tp2_fisica - largo_26.csv')
+    tl26 = [a-1 for a in tl26]
 
-    # print(thetal15.index(max(thetal15)))
-    # print(thetal26.index(max(thetal26)))
-
-    plot_trayectory_based_lenght([thetal15[46:], thetal26[51:], theta25[70:]])
+    plot_trayectory_based_lenght([tl15, tl26, t25], [thetal15[3:], thetal26[1:], theta25[7:]])
 
     tw5, rw5, thetaw5 = get_data('TP2/tp2_fisica - peso_madera.csv')
     tw72, rw72, thetaw72 = get_data('TP2/tp2_fisica - peso_oro.csv')
 
-    # print(thetaw5.index(max(thetaw5)))
-    # print(thetaw72.index(max(thetaw72)))
+    # plot_trayectory_based_weight([thetaw5[30:], theta25[70:], thetaw72[179:]])
 
-    plot_trayectory_based_weight([thetaw5[30:], theta25[70:], thetaw72[179:]])
+    # get_gravedad_local(1, [tl15, tl26, t25], [thetal15, thetal26, theta25])
 
 if __name__ == '__main__':
     main()
